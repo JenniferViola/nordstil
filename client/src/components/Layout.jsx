c9d77114c425861f33ecb0ac724cfe048c50dece
JJejja <jeenvian@gmail.com>
2025-12-12 17:57:45 +0100

    Add layout styling

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,120 +1,270 @@
 // /components/Layout.jsx
-import { useState } from "react";
-import { FaBars, FaHeart, FaMagnifyingGlass } from "react-icons/fa6";
+import { useState, useEffect } from "react";
+import {
+  FaBars,
+  FaHeart,
+  FaMagnifyingGlass,
+  FaBasketShopping,
+  FaXmark,
+  FaUser,
+} from "react-icons/fa6";
 
 export default function Layout(props) {
-  const [menuOpen, setMenuOpen] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
   const [searchOpen, setSearchOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  const headerOpen = menuOpen || searchOpen;
+
+  const siteLinks = ["Home", "Magazine", "About"];
+  const promotionLinks = ["New Arrivals", "Best Sellers", "Clothing"];
+  const userLinks = ["My Account", "Favorites"];
+
+  useEffect(() => {
+    const handleScroll = () => setIsScrolled(window.scrollY > 0);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  // Helper Component for Icons to reduce repetition
+  const ActionButton = ({ icon }) => (
+    <button
+      className="relative hover:scale-110 hover:text-primary-900 transition-all
+        duration-200 drop-shadow-md"
+    >
+      {icon}
+    </button>
+  );
 
   return (
-    <div id="layout" className="min-h-screen grid w-full">
-      <header className="bg-primary-500 text-secondary-500 w-full h-24 sm:h-34 border-b border-secondary-300 flex justify-center">
+    <div id="layout" className="grid min-h-screen w-full">
+      <header
+        className={`sticky top-0 z-50 w-full transition-all duration-300
+          bg-primary-500 text-secondary-500 border-b border-secondary-300/30 ${
+            isScrolled
+              ? "shadow-md py-1 backdrop-blur-md bg-primary-500/95"
+              : "py-2 sm:py-2"
+          } `}
+      >
         <div
-          id="header-inner"
-          className="mx-auto flex-row-reverse flex w-full max-w-[1440px] items-center justify-between"
+          id="header-container"
+          className="mx-auto grid max-w-[1400px] grid-cols-[1fr_auto_1fr]
+            items-center px-4 lg:px-6"
         >
-          <div id="header-actions" className="flex items-center gap-4">
-            <div
-              id="search"
-              className="rounded-sm flex items-center gap-2 active:scale-105 transition"
-            >
-              {/* -- FIX OPENED SEARCH INPUT OVERFLOWING -- */}
-              <button
-                id="searchBtnMobile"
-                className="sm:hidden"
-                onClick={() => setSearchOpen(!searchOpen)}
-              >
-                <FaMagnifyingGlass fill="#efefe6" size={20} />
-              </button>
-              <button id="searchBtnDesktop" className="hidden sm:block">
-                <FaMagnifyingGlass fill="#efefe6" size={18} />
-              </button>
-
-              <input
-                id="search"
-                type="text"
-                placeholder="Search..."
-                className={`${searchOpen ? "w-90" : "hidden"} sm:block text-secondary-100 border-b border-secondary-300/50 focus:outline-none focus:border-secondary-100 transition`}
-              />
-            </div>
-            <FaHeart
-              fill="#efefe6"
-              size={28}
-              viewBox="0 0 512 600"
-              style={{
-                filter: "drop-shadow(0 2px 4px rgba(0, 0, 0, 0.2))",
+          {/* NAVIGATION */}
+          <div id="nav-container" className="flex items-center justify-start">
+            <button
+              id="mobile-menu-button"
+              onClick={() => {
+                setMenuOpen(!menuOpen);
+                setSearchOpen(false);
               }}
-              className={`${searchOpen ? "hidden" : ""} transition hover:scale-110`}
+              className="lg:hidden p-2 -ml-2 hover:text-primary-900
+                transition-colors"
+              aria-label="Toggle Menu"
+            >
+              {menuOpen ? <FaXmark size={22} /> : <FaBars size={20} />}
+            </button>
+
+            {/* Desktop Nav */}
+            <nav id="desktop-nav" className="hidden lg:flex gap-8">
+              {["Home", "Shop", "About"].map((item) => (
+                <a
+                  key={item}
+                  href="/"
+                  className="group relative text-sm font-semibold tracking-wide
+                    hover:text-primary-900 transition-colors"
+                >
+                  {item}
+                  <span
+                    className="absolute -bottom-1 left-0 h-[2px] w-0
+                      bg-primary-900 transition-all duration-300
+                      group-hover:w-full"
+                  />
+                </a>
+              ))}
+            </nav>
+          </div>
+
+          {/* --- LOGO --- */}
+          <div id="logo-container" className="flex">
+            <a href="/" className="relative block group">
+              <img
+                src="images/nordstil-logo-header-light.webp"
+                alt="Nordstil"
+                className={`transition-all duration-300 mb-1
+                  ${isScrolled ? "h-10 lg:h-12" : "h-12 lg:h-14"}`}
+              />
+            </a>
+          </div>
+
+          {/* --- ACTIONS --- */}
+          <div
+            id="actions-container"
+            className="flex items-center justify-end gap-4"
+          >
+            <div
+              className="hidden lg:flex items-center transition-all duration-300
+                w-70"
+            >
+              {/* --- SEARCH - DESKTOP --- */}
+              <div
+                id="search-desktop"
+                className="relative w-full flex items-center"
+              >
+                <input
+                  type="text"
+                  placeholder="Search..."
+                  className="flex-1 pr-8 py-1 bg-transparent border-b
+                    border-secondary-500/50 text-sm text-secondary-100
+                    placeholder:text-secondary-500/50 focus:outline-none
+                    focus:border-secondary-100 transition-all duration-300
+                    ease-out"
+                />
+                <button
+                  className="z-10 hover:scale-110 transition-transform p-1"
+                >
+                  <FaMagnifyingGlass size={18} />
+                </button>
+              </div>
+            </div>
+
+            {/* --- SEARCH - MOBILE --- */}
+            <button
+              className="lg:hidden hover:scale-110 transition-transform"
+              onClick={() => {
+                setSearchOpen(!searchOpen);
+                setMenuOpen(false);
+              }}
+            >
+              <FaMagnifyingGlass size={18} />
+            </button>
+
+            {/* Icons */}
+            <div className="flex items-center gap-4 text-secondary-500">
+              <ActionButton icon={<FaHeart size={18} />} />
+              <ActionButton icon={<FaBasketShopping size={18} />} />
+            </div>
+          </div>
+        </div>
+
+        {/* --- MOBILE - SEARCH INPUT --- */}
+        <div
+          id="mobile-search-container"
+          className={`lg:hidden absolute left-0 top-full w-full bg-primary-500
+            overflow-hidden transition-all duration-250 border-b
+            border-secondary-300/20
+            ${searchOpen ? "max-h-20 opacity-100" : "max-h-0 opacity-0"}`}
+        >
+          <div className="px-4 py-3 flex items-center justify-center">
+            <input
+              autoFocus={searchOpen}
+              type="text"
+              placeholder="Search products..."
+              className="w-full max-w-md bg-secondary-100/10 rounded-xl px-4
+                py-2 text-sm text-secondary-100 focus:outline-none focus:ring-1
+                focus:ring-secondary-300"
             />
           </div>
+        </div>
 
-          <img
-            id="logo-image"
-            src="images/nordstil-logo-light.webp"
-            alt="Nordstil Logo"
-            className={`${searchOpen ? "hidden" : ""} w-40 h-auto sm:w-50 lg:w-52`}
-          />
+        {/* --- MOBILE - NAV --- */}
+        <div
+          id="mobile-nav-container"
+          className={`shadow-xl lg:hidden absolute left-0 top-full w-[60%]
+            min-h-screen bg-primary-500 text-secondary-400 transition-all
+            duration-250 ease-in-out
+            ${menuOpen ? "opacity-100" : "max-h-0 opacity-0"}`}
+        >
+          <nav
+            className="flex min-h-screen flex-col px-4 pt-6 pb-10 shadow-xl
+              gap-2"
+          >
+            <div id="site-links" className="flex flex-col gap-1">
+              {siteLinks.map((item) => (
+                <a
+                  key={item}
+                  href="/"
+                  className="w-full py-2.5 px-2 font-medium
+                    hover:text-primary-900 transition-colors"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
 
-          <div id="navigation">
-            <nav className="hidden sm:flex gap-3 text-sm sm:gap-6 sm:text-base">
-              <a
-                href="/"
-                className="hover:text-primary-900 text-xs font-semibold"
-              >
-                Home
-              </a>
-              <a
-                href="/"
-                className="hover:text-primary-900 text-xs font-semibold"
-              >
-                Shop
-              </a>
-              <a
-                href="/"
-                className="hover:text-primary-900 text-xs font-semibold"
-              >
-                About
-              </a>
-            </nav>
+            <div id="divider" className="border-t border-secondary-400/40" />
 
-            {/* -- Mobile -- */}
-            <button
-              className={`${searchOpen ? "hidden" : ""} sm:hidden`}
-              onClick={() => setMenuOpen(!menuOpen)}
-            >
-              <FaBars size={28} className="text-secondary-500" />
-            </button>
-            {menuOpen && (
-              <nav className="absolute right-0 z-10 w-full bg-primary-500 sm:hidden shadow-md py-6 px-4 flex flex-col gap-3 text-base">
+            <div id="promo-links" className="flex flex-col gap-1">
+              {promotionLinks.map((item) => (
                 <a
+                  key={item}
                   href="/"
-                  className="hover:text-primary-900 font-semibold border-b border-secondary-300/30"
+                  className="w-full py-2.5 px-2 font-medium
+                    hover:text-primary-900 transition-colors"
                 >
-                  Home
+                  {item}
                 </a>
+              ))}
+            </div>
+
+            <div id="user-links" className="mt-14 pt-4 flex flex-col gap-1">
+              <div className="flex items-center">
+                <FaUser
+                  fill="#e1e1d3"
+                  size={18}
+                  style={{
+                    filter: "drop-shadow(0 2px 4px rgba(0, 0, 0, 0.3))",
+                  }}
+                />
                 <a
+                  key={userLinks[0]}
                   href="/"
-                  className="hover:text-primary-900 font-semibold border-b border-secondary-300/30"
+                  className="py-3 px-2 text-sm font-light text-secondary-400
+                    hover:text-primary-900 transition-colors"
                 >
-                  Shop
+                  {userLinks[0]}
                 </a>
+              </div>
+              <div className="flex items-center">
+                <FaHeart
+                  fill="none"
+                  stroke="#e1e1d3"
+                  strokeWidth={52}
+                  size={20}
+                  style={{
+                    filter: "drop-shadow(0 2px 4px rgba(0, 0, 0, 0.3))",
+                  }}
+                  viewBox="0 0 512 600"
+                />
                 <a
+                  key={userLinks[1]}
                   href="/"
-                  className="hover:text-primary-900 font-semibold border-b border-secondary-300/30"
+                  className="py-3 px-2 text-sm font-extralight
+                    text-secondary-400 hover:text-primary-900 transition-colors"
                 >
-                  About
+                  {userLinks[1]}
                 </a>
-              </nav>
-            )}
-          </div>
+              </div>
+            </div>
+          </nav>
         </div>
       </header>
 
-      <main className="mx-auto md:w-full max-w-[2056px] grid gap-8">
+      <main
+        className="mx-auto grid max-w-[2056px] gap-8 md:w-full"
+        onClick={() => {
+          if (!headerOpen) return;
+          setSearchOpen(false);
+          setMenuOpen(false);
+        }}
+      >
         {props.children}
       </main>
-
-      <footer className="bg-primary-500 py-6 text-secondary-500 text-[0.8rem] border-t border-secondary-300 w-full">
+      <footer
+        className="w-full border-t border-secondary-300 bg-primary-500 py-6
+          text-[0.8rem] text-secondary-500"
+      >
         <div className="mx-auto max-w-[1200px] px-4 text-center">
           <p>&copy; 2025 Nordstil. All rights reserved.</p>
           <p className="mt-2">Designed with Scandinavian simplicity.</p>
--- client/src/pages/ThemeTest.jsx
+++ client/src/pages/ThemeTest.jsx
@@ -4,6 +4,195 @@ import Hero from "../components/Hero";
 function ThemeTest() {
   return (
     <section>
+      <header
+        className="relative w-full border-b border-secondary-300/60
+          bg-primary-500 text-secondary-500"
+      >
+        {/* --- LOGO --- */}
+        <div
+          id="img-container"
+          className={`${searchOpen ? "hidden" : ""} absolute left-1/2
+            -translate-x-1/2 flex justify-center items-center mb-2`}
+        >
+          <img
+            id="logo-image"
+            src="images/nordstil-logo-header-light.webp"
+            alt="Nordstil Logo"
+            className={`${searchOpen ? "hidden" : ""} h-auto w-30 sm:w-34
+              lg:w-38 pointer-events-none sm:pointer-events-auto`}
+          />
+        </div>
+
+        {/* --- INNER CONTAINER --- */}
+        <div
+          id="header-inner"
+          className={`${searchOpen ? "grid min-h-12" : "grid grid-cols-[auto_1fr_auto] min-h-18 px-4"}
+            mx-auto w-full max-w-[1400px] justify-center items-center`}
+        >
+          {/* --- NAVIGATION --- */}
+          <div
+            id="navigation"
+            className={`${searchOpen ? "hidden" : ""} flex justify-start`}
+          >
+            {/* --- Desktop --- */}
+            <nav className="hidden gap-3 text-sm lg:flex lg:gap-6 lg:text-base">
+              <a
+                href="/"
+                className="text-xs font-semibold hover:text-primary-900"
+              >
+                Home
+              </a>
+              <a
+                href="/"
+                className="text-xs font-semibold hover:text-primary-900"
+              >
+                Shop
+              </a>
+              <a
+                href="/"
+                className="text-xs font-semibold hover:text-primary-900"
+              >
+                About
+              </a>
+            </nav>
+
+            {/* -- Mobile -- */}
+            <div
+              id="mobile-menu-container"
+              className={`${searchOpen ? "hidden" : ""} lg:hidden flex flex-col
+                items-center w-full`}
+            >
+              <button
+                id="hamburger-button"
+                onClick={() => setMenuOpen(!menuOpen)}
+              >
+                <FaBars size={20} className="text-secondary-500" />
+              </button>
+            </div>
+          </div>
+
+          {/* --- ACTIONS --- */}
+          <div
+            id="header-actions"
+            className="flex justify-end items-center gap-8"
+          >
+            {/* --- SEARCH DESKTOP --- */}
+            <div
+              id="search-container"
+              className="flex items-center gap-2 lg:pb-1 transition lg:border-b
+                lg:border-secondary-300/50 active:scale-103"
+            >
+              <button
+                id="searchBtnMobile"
+                className={`${searchOpen ? "hidden" : ""} lg:hidden`}
+                onClick={() => setSearchOpen(true)}
+              >
+                <FaMagnifyingGlass fill="#efefe6" size={16} />
+              </button>
+              <button id="searchBtnDesktop" className="hidden lg:block">
+                <FaMagnifyingGlass fill="#efefe6" size={16} />
+              </button>
+
+              <input
+                id="search"
+                type="text"
+                placeholder="Search..."
+                className="hidden lg:block text-secondary-100 transition
+                  focus:outline-none"
+              />
+            </div>
+
+            {/* --- USER --- */}
+            <div id="basket-user-favorites" className="flex gap-4">
+              <FaUser
+                fill="#efefe6"
+                size={16}
+                style={{
+                  filter: "drop-shadow(0 2px 4px rgba(0, 0, 0, 0.2))",
+                }}
+                className={`${searchOpen ? "hidden" : ""} transition
+                  hover:scale-110`}
+              />
+              <FaHeart
+                fill="#efefe6"
+                size={16}
+                style={{
+                  filter: "drop-shadow(0 2px 4px rgba(0, 0, 0, 0.2))",
+                }}
+                className={`${searchOpen ? "hidden" : ""} transition
+                  hover:scale-110`}
+              />
+              <FaBasketShopping
+                fill="#efefe6"
+                size={16}
+                style={{
+                  filter: "drop-shadow(0 2px 4px rgba(0, 0, 0, 0.2))",
+                }}
+                className={`${searchOpen ? "hidden" : ""} transition
+                  hover:scale-110`}
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* --- MOBILE SEARCH --- */}
+        {searchOpen && (
+          <div
+            id="search-open"
+            className="lg:hidden absolute left-0 right-0 bottom-0 mx-auto w-full
+              max-w-[1400px] px-4 py-3"
+          >
+            <div className="flex items-center gap-2">
+              <button id="searchBtnMobile" className="lg:hidden">
+                <FaMagnifyingGlass fill="#efefe6" size={16} />
+              </button>
+              <input
+                type="text"
+                placeholder="Search..."
+                className="w-full border-b border-secondary-300/50
+                  bg-transparent text-secondary-100 focus:border-secondary-100
+                  focus:outline-none"
+              />
+            </div>
+          </div>
+        )}
+
+        {/* --- MOBILE NAV --- */}
+        {menuOpen && !searchOpen && (
+          <div
+            id="mobile-nav-container"
+            className="lg:hidden border-t border-secondary-300/30"
+          >
+            <nav
+              className="w-full grid gap-3 p-4 bg-primary-500 text-base
+                shadow-md"
+            >
+              <a
+                href="/"
+                className="border-b border-secondary-300/30 font-semibold
+                  hover:text-primary-900"
+              >
+                Home
+              </a>
+              <a
+                href="/"
+                className="border-b border-secondary-300/30 font-semibold
+                  hover:text-primary-900"
+              >
+                Shop
+              </a>
+              <a
+                href="/"
+                className="border-b border-secondary-300/30 font-semibold
+                  hover:text-primary-900"
+              >
+                About
+              </a>
+            </nav>
+          </div>
+        )}
+      </header>
+
       <Hero />
       <ProductGrid />
       <h1>h1 Heading</h1>
