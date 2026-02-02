65d27156a44b020db4e4c532b2f299469a9f3af9
JJejja <jeenvian@gmail.com>
2026-02-02 14:22:25 +0100

    Add first admin routes

--- client/src/app/pages/admin/Index.tsx
+++ client/src/app/pages/admin/Index.tsx
@@ -1,1 +1,13 @@
 // admin/Index.tsx
+import PageTitle from "@/components/layout/shared/PageTitle";
+
+export default function Index() {
+  return (
+    <>
+      <PageTitle title="Nordstil" />
+      <div>
+        <h1>Admin - Index</h1>
+      </div>
+    </>
+  );
+}
